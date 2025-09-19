3540e06902d35aa86585be5a1eb8271490ad2221
Gabrovez <gabrovez321@gmail.com>
2025-09-19 21:02:54 +0300

    //Задание 3

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
     internal class Program
     {
         //enum TrafficLight { Red, Yellow, Green }
-        public enum OrderStatus { New, Processing, Shipped, Delivered };
+        //public enum OrderStatus { New, Processing, Shipped, Delivered };
         static void Main(string[] args)
         {
             //Задание 1
@@ -19,22 +19,27 @@
             //TrafficLight nextColor = (TrafficLight)trafficLight.GetValue(i);
             //Console.WriteLine(nextColor); 
             //Задание 2
-            Console.Write("Введите статус заказа: ");
-            string input = Console.ReadLine();
-            OrderStatus stat = (OrderStatus)Enum.Parse(typeof(OrderStatus), input);
-            status_check((OrderStatus)stat);
-        }
-        public static void status_check(OrderStatus status)
-        {
-            switch (status)
+            //    Console.Write("Введите статус заказа: ");
+            //    string input = Console.ReadLine();
+            //    OrderStatus stat = (OrderStatus)Enum.Parse(typeof(OrderStatus), input);
+            //    status_check((OrderStatus)stat);
+            //}
+            //public static void status_check(OrderStatus status)
+            //{
+            //    switch (status)
+            //    {
+            //        case OrderStatus.New:
+            //        case OrderStatus.Processing:
+            //            Console.WriteLine("Заказ можно отменить");  
+            //            break;
+            //        default:
+            //            Console.WriteLine("Заказ нельзя отменить");
+            //            break;
+            //    }
+            //Задание 3
+            for (int i = 10; i > 0; i--)
             {
-                case OrderStatus.New:
-                case OrderStatus.Processing:
-                    Console.WriteLine("Заказ можно отменить");  
-                    break;
-                default:
-                    Console.WriteLine("Заказ нельзя отменить");
-                    break;
+                Console.WriteLine(i);
             }
         }
     }   
