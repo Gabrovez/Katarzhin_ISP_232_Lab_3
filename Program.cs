e3cbe09386038cb5559d2d4610a0007d36ab3f62
Gabrovez <gabrovez321@gmail.com>
2025-09-19 20:49:24 +0300

    //Задание 1

--- Program.cs
+++ Program.cs
@@ -2,9 +2,21 @@
 {
     internal class Program
     {
+        enum TrafficLight { Red, Yellow, Green }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Задание 1
+            var trafficLight = Enum.GetValues(typeof(TrafficLight));
+            int i = 0;
+            for (; i < trafficLight.Length; i++)
+            {
+                TrafficLight color = (TrafficLight)trafficLight.GetValue(i);
+                if (color == TrafficLight.Green)
+                    break;
+            }
+            i = (i + 1) % trafficLight.Length;
+            TrafficLight nextColor = (TrafficLight)trafficLight.GetValue(i);
+            Console.WriteLine(nextColor); 
         }
-    }
+    }   
 }
