56efd051e556dd50218822aa88bf9a53b53e010d
Gabrovez <gabrovez321@gmail.com>
2025-09-19 21:00:20 +0300

    //Задание 2

--- Program.cs
+++ Program.cs
@@ -2,21 +2,40 @@
 {
     internal class Program
     {
-        enum TrafficLight { Red, Yellow, Green }
+        //enum TrafficLight { Red, Yellow, Green }
+        public enum OrderStatus { New, Processing, Shipped, Delivered };
         static void Main(string[] args)
         {
             //Задание 1
-            var trafficLight = Enum.GetValues(typeof(TrafficLight));
-            int i = 0;
-            for (; i < trafficLight.Length; i++)
+            //var trafficLight = Enum.GetValues(typeof(TrafficLight));
+            //int i = 0;
+            //for (; i < trafficLight.Length; i++)
+            //{
+            //    TrafficLight color = (TrafficLight)trafficLight.GetValue(i);
+            //    if (color == TrafficLight.Green)
+            //        break;
+            //}
+            //i = (i + 1) % trafficLight.Length;
+            //TrafficLight nextColor = (TrafficLight)trafficLight.GetValue(i);
+            //Console.WriteLine(nextColor); 
+            //Задание 2
+            Console.Write("Введите статус заказа: ");
+            string input = Console.ReadLine();
+            OrderStatus stat = (OrderStatus)Enum.Parse(typeof(OrderStatus), input);
+            status_check((OrderStatus)stat);
+        }
+        public static void status_check(OrderStatus status)
+        {
+            switch (status)
             {
-                TrafficLight color = (TrafficLight)trafficLight.GetValue(i);
-                if (color == TrafficLight.Green)
+                case OrderStatus.New:
+                case OrderStatus.Processing:
+                    Console.WriteLine("Заказ можно отменить");  
+                    break;
+                default:
+                    Console.WriteLine("Заказ нельзя отменить");
                     break;
             }
-            i = (i + 1) % trafficLight.Length;
-            TrafficLight nextColor = (TrafficLight)trafficLight.GetValue(i);
-            Console.WriteLine(nextColor); 
         }
     }   
 }
