df64a61a418c857344c037b3b86574137ee19be9
Gabrovez <gabrovez321@gmail.com>
2025-09-27 00:28:19 +0300

    //Шаг 9. Перечисления 1

--- Program.cs
+++ Program.cs
@@ -1,41 +1,22 @@
 ﻿namespace Katarzhin_ISP_232_Lab_3
 {
-    internal class Program
+    public class Program
     {
+        enum DayOfWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
         //enum TrafficLight { Red, Yellow, Green }
         //public enum OrderStatus { New, Processing, Shipped, Delivered };
         static void Main(string[] args)
         {
-            //Задание 1
-            //var trafficLight = Enum.GetValues(typeof(TrafficLight));
-            //int i = 0;
-            //for (; i < trafficLight.Length; i++)
-            //{
-            //    TrafficLight color = (TrafficLight)trafficLight.GetValue(i);
-            //    if (color == TrafficLight.Green)
-            //        break;
-            //}
-            //i = (i + 1) % trafficLight.Length;
-            //TrafficLight nextColor = (TrafficLight)trafficLight.GetValue(i);
-            //Console.WriteLine(nextColor); 
-            //Задание 2
-            //    Console.Write("Введите статус заказа: ");
-            //    string input = Console.ReadLine();
-            //    OrderStatus stat = (OrderStatus)Enum.Parse(typeof(OrderStatus), input);
-            //    status_check((OrderStatus)stat);
-            //}
-            //public static void status_check(OrderStatus status)
-            //{
-            //    switch (status)
-            //    {
-            //        case OrderStatus.New:
-            //        case OrderStatus.Processing:
-            //            Console.WriteLine("Заказ можно отменить");  
-            //            break;
-            //        default:
-            //            Console.WriteLine("Заказ нельзя отменить");
-            //            break;
-            //    }
+
             //Задание 3
             //for (int i = 10; i > 0; i--)
             //{
@@ -129,22 +110,26 @@
             //    Console.WriteLine(i);
             //}
             //Шаг 8. Эхо-чат
-            string? input;
-            while (true)
-            {
-                Console.Write("> ");
-                input = Console.ReadLine();
-                if (input == null)
-                {
-                    continue;
-                }
-                if (input.ToLower() == "выход")
-                {
-                    Console.WriteLine("До встречи!");
-                    break;
-                }
-            }
-            Console.WriteLine($"Вы сказали: {input}");
+            //string? input;
+            //while (true)
+            //{
+            //    Console.Write("> ");
+            //    input = Console.ReadLine();
+            //    if (input == null)
+            //    {
+            //        continue;
+            //    }
+            //    if (input.ToLower() == "выход")
+            //    {
+            //        Console.WriteLine("До встречи!");
+            //        break;
+            //    }
+            //}
+            //Console.WriteLine($"Вы сказали: {input}");
+            //Шаг 9. Перечисления
+            DayOfWeek today = DayOfWeek.Friday;
+            Console.WriteLine(today); // Выведет "Friday"
+
         }
-    }   
+    }
 }
